ffa7c8f50edcc305a905386db2e851ab659cef84
Aljosha <aljoshajustin@gmail.com>
2021-06-21 17:43:21 +0200

    aa: added basic layout

--- frontend/android/SmallSocialAndroidApp/app/src/main/java/de/aljoshavieth/smallsocialandroidapp/MainActivity.java
+++ frontend/android/SmallSocialAndroidApp/app/src/main/java/de/aljoshavieth/smallsocialandroidapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    String apiBaseUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
